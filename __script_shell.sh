8c664a403b81731afae32cf6cb59b831f8eef7ca
Chevreau-Maxime <67205419+Chevreau-Maxime@users.noreply.github.com>
2020-10-13 16:33:11 +0200

    Initial commit + script shell

---
+++ __script_shell.sh
@@ -0,0 +1,9 @@
+echo "Starting execution of deepmatch for all images :"
+mkdir resultats
+let "numero = 1"
+for f in images/frag/*.ppm; do
+    echo "-> $f"
+    ./deepmatching-static $f images/fresque.ppm -out resultats/$numero.txt
+    let "numero = numero+1"
+    echo "done."
+done
