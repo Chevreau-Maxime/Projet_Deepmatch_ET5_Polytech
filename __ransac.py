cf3d3bcdd0490f0c870c1482fd6098dd494acba7
Chevreau-Maxime <67205419+Chevreau-Maxime@users.noreply.github.com>
2020-10-16 15:03:37 +0200

    cleaned code, added functions, read file .txt

--- __ransac.py
+++ __ransac.py
@@ -1,19 +1,50 @@
+import sys
 import numpy as np
 import array as arr
 from matplotlib import pyplot as plt
 from sklearn import linear_model, datasets
+import __ransac_functions as r_f
+
+
+# READ DATA FROM TXT FILE IN PARAM
+nb_paires = r_f.get_line_number(sys.argv[1])
+nb_infos = 6
+valeurs_txt = r_f.get_data_from_file(sys.argv[1])
+#print(valeurs_txt)
+
+x1 = np.zeros((nb_paires, 1))
+y1 = np.zeros((nb_paires, 1))
+x2 = np.zeros((nb_paires, 1))
+y2 = np.zeros((nb_paires, 1))
+for i in range(nb_paires):
+	x1[i] = valeurs_txt[i][0]
+	y1[i] = valeurs_txt[i][1]
+	x2[i] = valeurs_txt[i][2]
+	y2[i] = valeurs_txt[i][3]
+
+
+# GET RANSAC DATA
+tmp = r_f.do_ransac_on_data(x1, x2)
+line_x1 = tmp[0]
+line_y_ransac = tmp[1]
+ransac = tmp[2]
+
+# DISPLAY
+r_f.print_ransac(x1, x2, line_x1, line_y_ransac, ransac)
+
+
+
+
+
+
+""" OLD CODE
+
 
 n_samples = 50
 n_outliers = 5
-
-f = open("output.txt", "r")
-Lines = f.readlines() 
-for line in Lines:
-	# TODO
-	#count 0
-	line.strip()
-	
 X, y, coef = datasets.make_regression(n_samples=n_samples, n_features=1, n_informative=1, noise=10, coef=True, random_state=0)
+
+
 # Add outlier data
 # np.random.seed(0)
 # X[:n_outliers] = 3 + 0.5 * np.random.normal(size=(n_outliers, 1))
@@ -21,20 +52,19 @@ X, y, coef = datasets.make_regression(n_samples=n_samples, n_features=1, n_infor
 
 # Robustly fit linear model with RANSAC algorithm
 ransac = linear_model.RANSACRegressor()
-ransac.fit(X, y)
+ransac.fit(x1, x2)
 inlier_mask = ransac.inlier_mask_
 outlier_mask = np.logical_not(inlier_mask)
 
 # Predict data of estimated models
-line_X = np.arange(X.min(), X.max())[:, np.newaxis]
-line_y_ransac = ransac.predict(line_X)
+line_x1 = np.arange(x1.min(), x1.max())[:, np.newaxis]
+line_y_ransac = ransac.predict(line_x1)
 lw=2
-plt.scatter(X[inlier_mask], y[inlier_mask], color='yellowgreen', marker='.', label='Inliers')
-plt.scatter(X[outlier_mask], y[outlier_mask], color='gold', marker='.', label='Outliers')
-plt.plot(line_X, line_y_ransac, color='cornflowerblue', linewidth=lw, label='RANSAC regressor')
+plt.scatter(x1[inlier_mask], x2[inlier_mask], color='yellowgreen', marker='.', label='Inliers')
+plt.scatter(x1[outlier_mask], x2[outlier_mask], color='gold', marker='.', label='Outliers')
+plt.plot(line_x1, line_y_ransac, color='cornflowerblue', linewidth=lw, label='RANSAC regressor')
 plt.legend(loc='lower right')
 plt.xlabel("Input")
 plt.ylabel("Response")
 plt.show()
-
-f.close()
+"""
---
+++ __ransac_functions.py
@@ -0,0 +1,48 @@
+import numpy as np
+import array as arr
+from matplotlib import pyplot as plt
+from sklearn import linear_model, datasets
+
+def get_line_number(file_name):
+    with open(file_name) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+def get_data_from_file(file_name):
+    # Init np array
+    valeurs = np.zeros((get_line_number(file_name), 6))
+    idx_ligne = 0
+    f = open(file_name, "r")
+    Lines = f.readlines()
+    # Store lines
+    for line in Lines:
+        val_ligne = line.split(" ")
+        for i in range(6):
+            valeurs[idx_ligne][i] = float(val_ligne[i])
+        idx_ligne += 1
+    return valeurs
+
+
+def do_ransac_on_data(x1, x2):
+    # Apply ransac algorithm
+    ransac = linear_model.RANSACRegressor()
+    ransac.fit(x1, x2)
+    # Predict data of estimated models
+    x1_ransac = np.arange(x1.min(), x1.max())[:, np.newaxis]
+    x2_ransac = ransac.predict(x1_ransac)
+    return x1_ransac, x2_ransac, ransac
+
+
+def print_ransac(x1, x2, x1_ransac, x2_ransac, ransac):
+    # create 
+    inlier_mask = ransac.inlier_mask_
+    outlier_mask = np.logical_not(inlier_mask)
+    # 
+    plt.scatter(x1[inlier_mask], x2[inlier_mask], color='yellowgreen', marker='.', label='Inliers')
+    plt.scatter(x1[outlier_mask], x2[outlier_mask], color='gold', marker='.', label='Outliers')
+    plt.plot(x1_ransac, x2_ransac, color='cornflowerblue', linewidth=3, label='RANSAC regressor')
+    plt.legend(loc='lower right')
+    plt.xlabel("Input")
+    plt.ylabel("Response")
+    plt.show()
